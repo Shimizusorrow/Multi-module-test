a911fc533cbd524fc03d34d6ae96c85bb6c893ad
Shimizu <1047791704@qq.com>
2020-04-19 16:11:20 +0800

    starter 启动成功

--- starter/src/main/java/shimizu/statrter/StatrterApplication.java
+++ starter/src/main/java/shimizu/statrter/StatrterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.shimizu.common"
+        "shimizu.common"
 })
 //@EnableFeignClients
 public class StatrterApplication {
