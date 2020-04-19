d57aea0c1a77d9f03e012681bae4c0f1f7797e17
Shimizu <1047791704@qq.com>
2020-04-19 15:31:34 +0800

    create

---
+++ common/src/main/java/shimizu/common/swagger/swaggerConfig.java
@@ -0,0 +1,4 @@
+package shimizu.common.swagger;
+
+public class swaggerConfig {
+}
---
+++ starter/src/main/java/shimizu/statrter/StatrterApplication.java
@@ -0,0 +1,13 @@
+package shimizu.statrter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatrterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatrterApplication.class, args);
+    }
+
+}
