c7f586ba5cf1b406227536ea7d2332958ba5d522
Shimizu <1047791704@qq.com>
2020-04-20 13:37:59 +0800

    1.connect app to base 2.create app and connect to database

--- starter/src/main/java/shimizu/statrter/StatrterApplication.java
+++ starter/src/main/java/shimizu/statrter/StatrterApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "shimizu.common"
+        "shimizu.common",
+        "shimizu.app"
 })
 //@EnableFeignClients
 public class StatrterApplication {
