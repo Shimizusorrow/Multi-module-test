550a1ab693fc5a3ebea91c7e0d9d007a5415eeca
Shimizu <1047791704@qq.com>
2020-04-20 11:01:44 +0800

    create application

---
+++ app/src/test/java/shimizu/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package shimizu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
